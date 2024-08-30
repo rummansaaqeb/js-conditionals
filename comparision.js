d83323bc75fe56b49d369be43ab63e2b33612418
rummansaaqeb <rsaaqeb@gmail.com>
2024-08-30 12:26:54 +0600

    javascript conditionals

---
+++ comparision.js
@@ -0,0 +1,25 @@
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log(5 == 10);
+// console.log(10 == 10);
+// console.log(5 >= 10);
+// console.log(100 <= 10);
+
+// console.log(10 == 10.1);
+
+// -------------------------------
+var peyaraPrice = 40;
+var applePrice = 250;
+// console.log(applePrice > peyaraPrice)
+// console.log(peyaraPrice > applePrice) 
+/**
+ * ==
+ * ===
+ */
+
+// console.log(10 == "10")
+// console.log(10 === "10")
+console.log(10 !== "10")
+console.log(10 !== '10')
+
+
---
+++ variable.js
@@ -0,0 +1,35 @@
+/**
+ * VARIABLE ===> container
+ * 
+ */
+
+// Numeric
+var sunglassPrice = 80;
+
+// String
+var subject = "EEE";
+
+// Boolean
+var passed = true;
+
+
+/**
+ * NAMING CONVENTION 
+ * 1. single word 
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. can not start with a number
+ * 5. cannot be a keyword or reserved word
+ * 6. prefer camelCase
+ */
+
+var lastExamResult = 89;
+
+/**
+ * OPERATION
+ * 1. +, -, *, /, %
+ * 2. +=, -=, *=, /=
+ * 3. isNaN ?
+ * 4. null
+ * 5. Number
+ */
